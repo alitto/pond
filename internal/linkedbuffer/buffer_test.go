cba69f622344d00d272574b592466a77aa0a9e63
Alejandro Durante <alitto@gmail.com>
2025-04-16 09:35:15 -0300

    fix(buffer): avoid holding references to submitted tasks

--- internal/linkedbuffer/buffer_test.go
+++ internal/linkedbuffer/buffer_test.go
@@ -1,6 +1,8 @@
 package linkedbuffer
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/alitto/pond/v2/internal/assert"
@@ -37,4 +39,71 @@ func TestBuffer(t *testing.T) {
 	value, err = buffer.Read()
 	assert.Equal(t, 0, value)
 	assert.Equal(t, ErrEOF, err)
+}
+
+func TestBufferWithPointerToLargeObject(t *testing.T) {
+	var m runtime.MemStats
+
+	type Payload struct {
+		data []byte
+	}
+
+	runtime.ReadMemStats(&m)
+	before := int64(m.Alloc)
+
+	dataSize := 10 * 1024 * 1024
+	data := &Payload{
+		data: make([]byte, dataSize),
+	}
+
+	runtime.ReadMemStats(&m)
+	after := int64(m.Alloc)
+
+	// Verify large object allocated memory
+	assert.Equal(t, true, after-before >= int64(dataSize))
+
+	buffer := newBuffer[*Payload](10)
+
+	runtime.ReadMemStats(&m)
+	before = int64(m.Alloc)
+
+	buffer.Write(data)
+
+	runtime.ReadMemStats(&m)
+	after = int64(m.Alloc)
+
+	// Verify large object wasn't copied when writing it to the buffer
+	assert.Equal(t, true, after-before < int64(dataSize))
+
+	runtime.ReadMemStats(&m)
+	before = int64(m.Alloc)
+
+	readData, err := buffer.Read()
+
+	runtime.ReadMemStats(&m)
+	after = int64(m.Alloc)
+
+	// Verify large object wasn't copied while reading
+	assert.Equal(t, nil, err)
+	assert.Equal(t, cap(data.data), cap(readData.data))
+	assert.Equal(t, true, after-before < int64(dataSize))
+
+	runtime.ReadMemStats(&m)
+	before = int64(m.Alloc)
+
+	// Remove references to the large object
+	data.data = make([]byte, 0)
+	readData.data = make([]byte, 0)
+
+	// Trigger garbage collection
+	runtime.GC()
+
+	runtime.ReadMemStats(&m)
+	after = int64(m.Alloc)
+
+	// Verify large object was garbage collected (no internal references to the large object remain)
+	assert.Equal(t, true, before-after >= int64(dataSize))
+
+	// Keep a reference to these variables to ensure they are not discarded by GC
+	fmt.Printf("%#v, %#v\n", data.data, readData.data)
 }
